d8a3aaab10d5575a28042ea3fd12483476a785bf
souvik-biswas-dev <souvikbiswas.dev@gmail.com>
2026-03-30 16:15:19 +0530

    nginx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://18.233.224.24:3000',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
